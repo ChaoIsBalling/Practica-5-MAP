bd75b07b2724f1c57778032443058c62915f2c54
JialeHe28 <114423476+JialeHe28@users.noreply.github.com>
2023-04-24 11:13:22 +0200

    New

---
+++ Practica 5 MAP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica_5_MAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
